3145d1159457210a4fcbb66244a1167bd1d5ede1
Albert <wlj_3322@163.com>
2024-02-27 18:33:51 +0800

    feat: find previous tag by prefix

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,35 @@ async function run() {
 
   const sourceTag = core.getInput("source_tag", { required: true });
   const escapeBreak = core.getInput("escape_break");
+  const prefix = core.getInput("prefix");
+
+  let prevTag = "";
 
   // const { owner, repo } = github.context.repo;
   const { owner, repo } = { owner: "surreal-ai", repo: "pacific" };
   const client = github.getOctokit(GITHUB_TOKEN);
 
+  if (prefix) {
+    try {
+      const releases = await client.rest.repos.listReleases({
+        owner,
+        repo,
+        per_page: 50,
+      });
+      const findRelease = releases.data.find((rl) =>
+        rl.tag_name.startsWith(prefix)
+      );
+      if (findRelease) {
+        prevTag = findRelease.tag_name;
+      }
+    } catch (error) {}
+  }
+
   const response = await client.rest.repos.generateReleaseNotes({
     owner,
     repo,
     tag_name: sourceTag,
+    previous_tag_name: prevTag ? prevTag : void 0,
   });
   const notes = response.data.body;
   let parsedNotes = parseReleaseNote(notes, { owner, repo });
